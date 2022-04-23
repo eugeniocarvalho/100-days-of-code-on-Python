aaf5885a66530496097ef339b5fea83102e7f197
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-04-23 20:39:53 -0300

    correções

--- day-016/main.py
+++ day-016/main.py
@@ -10,19 +10,19 @@ coffee_order = ""
 
 
 while coffee_order != 'off':
-  coffee_order = input("What would you like? (espresso/latte/cappuccino) ")
+  coffee_order = input(f"What would you like? ({menu.get_items()}) ")
 
   if (coffee_order == 'report'):
     coffee_maker.report()
     money.report()
   elif coffee_order != 'off':
     drink = menu.find_drink(coffee_order)
+    
     if drink:
       flag = coffee_maker.is_resource_sufficient(drink)
 
-      if flag:
+      if flag and money.make_payment(drink.cost):
         coffee_maker.make_coffee(drink)
-        money.make_payment(drink.cost)
     else:
       break
   else:
