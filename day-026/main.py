76d0710c97a7ad46cbd4111d10a8e1b06ec1c282
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-06-04 20:56:48 -0300

    updated the challenge using error handling

--- day-026/main.py
+++ day-026/main.py
@@ -3,9 +3,14 @@ import pandas
 data = pandas.read_csv("/home/eugenio/Projetos/Udemy/100-days-of-code-on-Python/day-026/nato_phonetic_alphabet.csv") 
 dictionary = {row.letter:row.code for (index, row) in data.iterrows()}
 
-name = input("Enter a word:")
-name = [letter for letter in name]
+def generator_nato():
+  name = input("Enter a word:").upper()
+  try:
+    nato = [dictionary[letter] for letter in name ]
+  except KeyError:
+    print("Sorry, only letters in the alphabet please")
+    generator_nato()
+  else:
+    print(nato)
 
-nato = [dictionary[letter.upper()] for letter in name ]
-
-print(nato)
+generator_nato()
