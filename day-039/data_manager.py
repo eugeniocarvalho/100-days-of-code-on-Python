39d8ed80e1f972e947d1e67c81f31f30ce247226
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-06-15 17:08:30 -0300

    #day39 part2

--- day-039/data_manager.py
+++ day-039/data_manager.py
@@ -1,24 +1,34 @@
+from pprint import pprint
 import requests
 import os
 from dotenv import load_dotenv
-from pprint import pprint
 
 load_dotenv()
 
-AUTH = os.getenv('SHEETY_AUTH')
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/09ce695de0902f26460a4e0de71ffea7/flightDeals/prices/"
 
-headers = {
-  'Authorization': AUTH
-}
-
-url = "https://api.sheety.co/09ce695de0902f26460a4e0de71ffea7/flightDeals/prices"
 
 class DataManager:
-  def __init__(self):
-    response = requests.get(url=url, headers=headers)
 
-    self.sheet_data = response.json()["prices"]
-  
-  
-  def get__data(self):
-    return self.sheet_data
+  def __init__(self):
+    self.destination_data = {}
+
+  def get_destination_data(self):
+    response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+    data = response.json()
+    print(data)
+    self.destination_data = data["prices"]
+    return self.destination_data
+
+  def update_destination_codes(self):
+    for city in self.destination_data:
+      new_data = {
+        "price": {
+          "iataCode": city["iataCode"]
+        }
+      }
+      response = requests.put(
+        url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+        json=new_data
+      )
+      print(response.text)
--- day-039/flight_search.py
+++ day-039/flight_search.py
@@ -1,36 +1,60 @@
 import requests
-import os
+from flight_data import FlightData
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-headers = {
-  'Authorization': os.getenv('SHEETY_AUTH')
-}
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = os.getenv("TEQUILA_API_KEY")
+
 
 class FlightSearch:
-  def __init__(self, city):
-    url = f"https://api.sheety.co/09ce695de0902f26460a4e0de71ffea7/flightDeals/prices/{city['id']}"
-    
-    flight_params = {
-      "term": city["city"],
+
+  def get_destination_code(self, city_name):
+    location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+    headers = {"apikey": TEQUILA_API_KEY}
+    query = {"term": city_name, "location_types": "city"}
+    response = requests.get(url=location_endpoint, headers=headers, params=query)
+    results = response.json()["locations"]
+    code = results[0]["code"]
+    return code
+
+  def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+    headers = {"apikey": TEQUILA_API_KEY}
+    query = {
+      "fly_from": origin_city_code,
+      "fly_to": destination_city_code,
+      "date_from": from_time.strftime("%d/%m/%Y"),
+      "date_to": to_time.strftime("%d/%m/%Y"),
+      "nights_in_dst_from": 7,
+      "nights_in_dst_to": 28,
+      "flight_type": "round",
+      "one_for_city": 1,
+      "max_stopovers": 0,
+      "curr": "GBP"
     }
-    
-    header = {
-      "apikey": os.getenv('TEQUILA_API_KEY'),
-    }
-    
-    flight_url = "https://tequila-api.kiwi.com/locations/query"
-    
-    response = requests.get(url=flight_url, params=flight_params, headers=header)
-    
-    iata_code = response.json()["locations"][:1][0]["code"]
-    
-    body = {
-      "price": {
-        'iataCode': iata_code
-      }
-    }
-    
-    requests.put(url=url, json=body, headers=headers)
-    
+
+    response = requests.get(
+      url=f"{TEQUILA_ENDPOINT}/v2/search",
+      headers=headers,
+      params=query,
+    )
+
+    try:
+      data = response.json()["data"][0]
+    except IndexError:
+      print(f"No flights found for {destination_city_code}.")
+      return None
+
+    flight_data = FlightData(
+      price=data["price"],
+      origin_city=data["route"][0]["cityFrom"],
+      origin_airport=data["route"][0]["flyFrom"],
+      destination_city=data["route"][0]["cityTo"],
+      destination_airport=data["route"][0]["flyTo"],
+      out_date=data["route"][0]["local_departure"].split("T")[0],
+      return_date=data["route"][1]["local_departure"].split("T")[0]
+    )
+    print(f"{flight_data.destination_city}: £{flight_data.price}")
+    return flight_data
