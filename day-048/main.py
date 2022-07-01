5e5b1e3a6ae72c6648694daafdb1ce9bbfe5715a
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-07-01 20:31:50 -0300

    update path

--- day-048/main.py
+++ day-048/main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-chrome_driver_path = YOUR CHROME DRIVER PATH
+chrome_driver_path = "/usr/bin/chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
