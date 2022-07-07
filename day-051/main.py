3d7cc30c292703674673bbe0b6604429ca5d3371
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-07-07 19:53:04 -0300

    update main

--- day-051/main.py
+++ day-051/main.py
@@ -5,7 +5,4 @@ from InternetSpeedTwitterBot import InternetSpeedTwitterBot
 my_bot = InternetSpeedTwitterBot()
 
 my_bot.get_internet_speed()
-
-print(my_bot.down)
-print(my_bot.up)
-print(my_bot.ping)
+my_bot.tweet_at_provider()
